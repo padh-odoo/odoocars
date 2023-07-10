ab04a9e38926bfa62d70c0da48715a30fd10fd2f
padh-odoo <padh@odoo.com>
2023-07-10 18:36:02 +0530

    Sequence & Inine View Added

--- models/car_category.py
+++ models/car_category.py
@@ -2,7 +2,9 @@ from odoo import fields,models
 class car_categories(models.Model):
     _name="car.categories"
     _description="Cars Categories"
+    _sequence="sequence"
     
+    sequence=fields.Integer("Sequence",default=1)
     name=fields.Char("Title")
     category_ids=fields.One2many("car.feature",'category')
     _sql_constraints=[('Check_Name','unique(name)','Category name already exist')]
