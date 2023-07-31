8d18575c4d095f6cee8f717db31b6eb528b0426a
padh-odoo <padh@odoo.com>
2023-07-31 10:36:08 +0530

    Code Refactored

--- models/cars_view.py
+++ models/cars_view.py
@@ -10,11 +10,11 @@ class car_feature(models.Model):
     
     name=fields.Char("Car",required=True)
     description=fields.Text("Description")
-    selling_price=fields.Integer("Selling Price",tracking=True)
+    selling_price=fields.Integer("Selling Price",tracking=True,default=500000)
     engine=fields.Integer("Engine")
     boot_space=fields.Integer("Boot Space")
     seating_capacity=fields.Integer("Seating Capacity")
-    state=fields.Selection(selection=[("INS","REGISTERED"),("S","APPLIED")],default="INS")
+    state=fields.Selection(selection=[("INS","IN STOCK"),("S","SOLD")],default="INS")
     mileage=fields.Integer("Mileage")
     model=fields.Char("Model",tracking=True)
     transmission_type=fields.Selection(selection=[("manual","Manual"),("auto","Automatic")], string="Transmission Type",default="manual")
@@ -23,16 +23,14 @@ class car_feature(models.Model):
     brand=fields.Many2one("brand.type",string="Car Brand")
     offer_id=fields.Many2many("car.offer",string="Offer")
     image=fields.Image("Image")
-    registration_number=fields.Char("Registration Number")
-    chassis_number=fields.Char("Chassis Number")
+    registration_number=fields.Char("Registration Number",default="UP 65 AT 5376")
+    chassis_number=fields.Char("Chassis Number",default="2138675543321")
     length=fields.Float("Length")
     gst=fields.Selection(selection=[('gst12',"GST 12%"),("gst28","GST 28%")],string="GST")
     total_airbags=fields.Integer("Total AirBags",compute="_compute_airbags",store=True)
     salesperson_id = fields.Many2one('res.users', string='Salesperson', index=True, default=lambda self: self.env.user)
     buyer_id = fields.Many2one('res.partner', string='Customer', index=True)
 
-
-    
     # bool fields
     fog_lights=fields.Boolean("Fog Lights")
     airbag=fields.Boolean("Air Bag")
@@ -40,7 +38,6 @@ class car_feature(models.Model):
     alloys=fields.Boolean("Alloys")
     abs=fields.Boolean("ABS")
     total=fields.Integer("Total Price",compute="_total_price",store=True)
-    
     
     # Fiels for odoo wrap
     employee_id = fields.Many2one('hr.employee', string="Employee")
@@ -54,15 +51,12 @@ class car_feature(models.Model):
     wrap=fields.Boolean("Wrap",default=False)
     wraping=fields.Selection(selection=[("F","Full Wraping"),("L","Light Wraping")])
     amount=fields.Integer("Amount", compute="_calculate_amount")
-
-    
     
     #To calculate renewal deadline
     @api.depends('tenure','create_date')
     def _create_deadline(self):
         for i in self:
-            i.date_deadline = ( (i.create_date.date() if i.create_date else fields.Date.today()) + relativedelta(years=i.tenure))
-            
+            i.date_deadline = ( (i.create_date.date() if i.create_date else fields.Date.today()) + relativedelta(years=i.tenure))     
     
     # To calculate price to be paid
     @api.depends('wraping')
@@ -75,9 +69,7 @@ class car_feature(models.Model):
             else:
                 i.amount=0
 
-
     # To calculate total airbags
-    
     @api.depends('airbag','airbag_passenger')
     def _compute_airbags(self):
         for i in self:
@@ -87,8 +79,7 @@ class car_feature(models.Model):
                     i.total_airbags+=4
             else:
                 i.total_airbags=0
-
-            
+      
     def action_sold(self):
         print(".................................", self.employee_id.name)
         if self.state=="INS":
@@ -96,10 +87,8 @@ class car_feature(models.Model):
         else:
          raise UserError("You cannot sold")
      
-     
-     
     # To calculate price after tax inclusion and  offer
-    @api.depends('fuel_id','selling_price','gst','offer_id')
+    @api.depends('fuel_id','selling_price','gst','offer_id','wrap','wraping')
     def _total_price(self):
         for i in self:
             if (i.fuel_id=="H"):
@@ -107,12 +96,15 @@ class car_feature(models.Model):
                 i.gst='gst12'  
             else:
                 i.total= i.selling_price +i.selling_price*28/100
-                i.gst="gst28" 
-            """if i.offer_id:
-               if (i.offer_id.name=="Launch"):
-                i.total=i.selling_price-i.selling_price*10/100
-               elif (i.offer_id.name=="Festive"):
-                i.total=i.selling_price-i.selling_price*5/100 """
+                i.gst='gst28'
+                
+            if i.wrap:
+                if i.wraping=='F':
+                    i.total-=100000
+                elif i.wraping=="L":
+                    i.total-=20000
+                else:
+                    i.total=i.total
                 
     # Python Constrints
     @api.constrains('selling_price')
@@ -120,7 +112,7 @@ class car_feature(models.Model):
         if self.selling_price<0:
             raise ValidationError("Selling Price Must be Positive")
     
-    
+    #wizard
     def wizard(self):
         return{
 		    'type': 'ir.actions.act_window',
